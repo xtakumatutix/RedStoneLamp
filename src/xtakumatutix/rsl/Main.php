0f3a8f3f2bc309878da61f58e2c3caebc223e145
MyDeacy <mydeacy01010@gmail.com>
2022-01-16 20:09:40 +0900

    ランプを消すにも対応 & 簡略化

--- src/xtakumatutix/rsl/Main.php
+++ src/xtakumatutix/rsl/Main.php
@@ -2,34 +2,23 @@
 
 namespace xtakumatutix\rsl;
 
-use pocketmine\plugin\PluginBase;
-
-use pocketmine\block\Block;
-use pocketmine\block\VanillaBlocks;
-use pocketmine\block\BlockFactory;
-use pocketmine\math\Vector3;
-
+use pocketmine\block\RedstoneLamp;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\plugin\PluginBase;
 
-Class Main extends PluginBase implements Listener
-{
-    public function onEnable() :void
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+class Main extends PluginBase implements Listener {
 
-    public function onPlace(PlayerInteractEvent $event)
-    {
-        $block = $event->getBlock();
-        if ($block->getID() == 123){
-            $factory = BlockFactory::getInstance();
-            $player = $event->getPlayer();
-            $vector = $block->getPosition()->asVector3();
-            $world = $player->getPosition()->getWorld();
-            $lamp = $factory->get(124,0);
-            $world->setBlock($vector, $lamp);
-            $player->sendPopup("§bレッドストーンランプをつけたよ！");
-        }
-    }
+	public function onEnable() :void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onPlace(PlayerInteractEvent $event) {
+		$block = $event->getBlock();
+		if ($block instanceof RedstoneLamp) {
+			$power = $block->isPowered();
+			$block->setPowered(!$power);
+			$event->getPlayer()->sendPopup("§bレッドストーンランプを" . ($power ? "消したよ！" : "つけたよ！"));
+		}
+	}
 }
